2c183c80b3d72f9da95ded4c48fd21f990052e99
James Wang <jameswang9909@hotmail.com>
2020-04-09 15:18:23 -0400

    Fix inverted conditional

--- app/static/js/admin/attractions.js
+++ app/static/js/admin/attractions.js
@@ -5,9 +5,9 @@ $("form").submit(function(e) {
     let attraction_id = form.attr("id");
     let name = $(`#${attraction_id}-name`).val();
     let address = $(`#${attraction_id}-addr`).val();
-    let rating = $(`#${attraction_id}-rating`).val();
-    let latitude = $(`#${attraction_id}-lat`).val();
-    let longitude = $(`#${attraction_id}-long`).val();
+    let rating = parseFloat($(`#${attraction_id}-rating`).val());
+    let latitude = parseFloat($(`#${attraction_id}-lat`).val());
+    let longitude = parseFloat($(`#${attraction_id}-long`).val());
     $(`#${attraction_id}-submit`).attr("disabled", "disabled");
     apiCall("POST", `/api/attractions/${attraction_id}`, {
         name: name,
