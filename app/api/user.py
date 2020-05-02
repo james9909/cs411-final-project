4b5b0c40236ca830afc194d6a5f3e3dd5a57d3c4
Jundong Chen <jundong2@illinois.edu.com>
2020-05-01 20:20:04 -0500

    Modify login&register blueprints and base layout to support error messages and redirect

--- app/api/user.py
+++ app/api/user.py
@@ -13,33 +13,45 @@ def register():
     username = request.form["username"]
     password = request.form["password"]
     re_password = request.form["re_password"]
+    error = None
 
-    if len(password) < 4:
-        return {"message": "Password should be at least 4 characters long"}
-    if password != re_password:
-        return {"message": "Passwords don't match. Try again."}
-    result = db.session.execute("SELECT 1 FROM users WHERE username = :username", {"username": username}).fetchone()
-    if result is not None:
-        return {"message": "That username is taken"}
-    db.session.execute("INSERT INTO users (username, password, is_admin) VALUES (:username, :password, 0)", {
-        "username": username,
-        "password": hashlib.sha256(password.encode("utf-8")).hexdigest()
-    })
-    db.session.commit()
-    return {"message": "Success!"}
+    if not username:
+        error = "Username is required."
+    elif len(password) < 4:
+        error = "Password should be at least 4 characters long."
+    elif password != re_password:
+        error = "Passwords don't match. Try again."
+    elif db.session.execute("SELECT 1 FROM users WHERE username = :username", {"username": username}).fetchone() is not None:
+        error = "User {} is already registered.".format(username)
+
+    if error is None:
+        db.session.execute("INSERT INTO users (username, password, is_admin) VALUES (:username, :password, 0)",
+            {
+                "username": username,
+                "password": hashlib.sha256(password.encode("utf-8")).hexdigest()
+            })
+        db.session.commit()
+        return redirect(url_for("login"))
+
+    flash(error)
+    return render_template("register.html")
 
 @blueprint.route("/login", methods=["POST"])
 @api_view
 def login():
     username = request.form["username"]
     password = request.form["password"]
+    error = None
 
     result = db.session.execute("SELECT id, password, is_admin FROM users WHERE username = :username AND password = :password", {
         "username": username,
         "password": hashlib.sha256(password.encode("utf-8")).hexdigest()
     }).fetchone()
     if result is None:
-        return {"message": "Invalid credentials"}
+        error = "Invalid credentials"
+        flash(error)
+        return render_template("login.html")
+
     session["uid"] = result[0]
     session["is_admin"] = result[2] == 1
-    return {"message": "Success!"}
+    return redirect(url_for("index"))
